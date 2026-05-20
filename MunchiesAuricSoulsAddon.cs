1353a96596c6926a793a57685bf8328759e5ee3a
Pudd1ng <164870708+Manelich1107@users.noreply.github.com>
2026-05-20 21:45:53 +0800

    hwr logic fix

--- MunchiesAuricSoulsAddon.cs
+++ MunchiesAuricSoulsAddon.cs
@@ -187,7 +187,7 @@ public class MunchiesAuricSoulsAddon : Mod
         RegisterConsumable(munchies, coj, "HotCase", "OtherUpgradesPlayer", "HotCase", Color.White, category: "Homeward Journey", isIntCheck: true);
         RegisterConsumable(munchies, coj, "WhimInABottle", "OtherUpgradesPlayer", "WhimInABottle", Color.White, category: "Homeward Journey", isIntCheck: true);
         RegisterConsumable(munchies, coj, "HeartOfOcean", "OtherUpgradesPlayer", "HeartOfOcean", Color.White, category: "Homeward Journey", isIntCheck: true);
-        if (!ModLoader.TryGetMod("HomewardRagnarok", out Mod hr) || GetModConfigValue<bool>(hr, "ServerConfig", "PermanentToAccessories"))
+        if (!ModLoader.TryGetMod("HomewardRagnarok", out Mod hr) && GetModConfigValue<bool>(hr, "ServerConfig", "PermanentToAccessories"))
         {
             RegisterConsumable(munchies, coj, "TheSwitch", "OtherUpgradesPlayer", "TheSwitch", Color.White, category: "Homeward Journey", isIntCheck: true);
         }
